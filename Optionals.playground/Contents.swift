91dc0aae83a5996a45f99cc562f35020305b5747
zainnadeem <zn.nadeem@gmail.com>
2016-07-08 14:49:34 -0400

    func readme

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -48,3 +48,6 @@ func produceFullName(withFirstName firstName: String, middleName: String?, lastN
 
 produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
 produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
+
+
+//Optional parameters to functions can be very useful and powerful. Of course, if your function takes an Optional as a parameter, you are forced to deal with cases in which nil is passed, so you should only take an Optional parameter when you are prepared to deal with cases in which the value is nil.
